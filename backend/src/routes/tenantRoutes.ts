a8216a8112bde11a87b0b476245b7cc7894c6b63
Nuril Qolbi Zam Zami <147578806+Obay13579@users.noreply.github.com>
2025-12-22 13:44:21 +0700

    Delete tenant routes as part of backend cleanup

--- backend/src/routes/tenantRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getTenants, registerTenant } from '../controllers/tenantController';
-
-const router = Router();
-
-router.get('/', getTenants);      // View all
-router.post('/', registerTenant); // Create new
-
-export default router;
